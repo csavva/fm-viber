cfaa01b73fd84e83ff3ee8cbb1133836c23d527a
Christos Savva <csavva@velistar.com>
2020-02-28 08:07:08 +0200

    feat: Add settings file
    Created a clean settings file to store the credentials. No data will be saved in this file on Git following best practices. 
    
    Create a separate file called 'mysettings.php' to store credentials locally. This file is on the gitnore list.

---
+++ settings.php
@@ -0,0 +1,12 @@
+<?php
+
+  // add FileMaker database settings here
+  // remember to enable Data API priviledges for the user and the layout(s) needed
+
+  $host = '';
+  $db = '';
+  $user = '';
+  $pass = '';
+  $layout = '';
+
+?>
